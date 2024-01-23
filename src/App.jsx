1b3b18896159b565fac6f15cbd0dc360ed79e22d
sheyla astorga <sheylaastorga1998@gmail.com>
2024-01-23 19:25:12 -0300

    se modifico el texto del props

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Helloworld2 from "./components/helloWorld";
 function App() {
   return <>
   <section>
-   <Helloworld2 textoAdicional = "my Friend!"></Helloworld2>
+   <Helloworld2 textoAdicional = "my friend!"></Helloworld2>
   </section></>;
 }
 
